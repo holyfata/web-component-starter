4c1aba487bf1e54d28c12524802c9b507d382bf9
holyfata <holyfata@outlook.com>
2025-04-12 20:54:53 +0800

    feat: link package and app

--- apps/app-vue/src/App.vue
+++ apps/app-vue/src/App.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
-const title = ref<string>("Rspack + Vue");
+import { Counter } from "package-vue"
 </script>
 
 <template>
 	<div>
-		<a href="https://www.rspack.dev/" target="_blank">
-			<img src="./assets/rspack.svg" class="logo" alt="Rspack logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
+		<Counter />
 	</div>
-	<HelloWorld :msg="title" />
 </template>
 
 <style scoped>
