d72760c76951cbe3f554a1b1722f5169cf433ff9
heyongqi10 <heyongqi10@jd.com>
2025-07-18 16:17:42 +0800

    feat: 单项目测试vue和react组件

---
+++ app/src/containers/ReactContainer.tsx
@@ -0,0 +1,12 @@
+import { HfQrcode } from "package-react";
+
+const ReactContainer = () => {
+  return (
+    <div>
+      <h1>Hello from ReactContainer</h1>
+      <HfQrcode></HfQrcode>
+    </div>
+  );
+};
+
+export default ReactContainer;
---
+++ app/src/containers/VueContainer.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { HfQrcode } from "package-vue";
+</script>
+
+<template>
+  <div>
+    <h1>Hello from VueContainer</h1>
+    <HfQrcode></HfQrcode>
+  </div>
+</template>
